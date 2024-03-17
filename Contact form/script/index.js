6d49606ba53bf179ed114db2d9e54d96e509e4ef
Manigandan <54367953+iamurmani@users.noreply.github.com>
2024-03-17 22:48:27 +0530

    contact form

---
+++ Contact form/script/index.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  fetch("https://formspree.io/f/mkndzekr", {
+    method: 'POST',
+    body: data,
+  }).then(response => {
+    if (response.ok) {
+      alert('Success');
+    } else {
+      alert('Error');
+    }
+  });
+  document.getElementById('contact-form').reset();
+});
